f6bad9d81c7f39ab7bb17c2db8008690dd7502bc
KS <bye_ok@naver.com>
2020-08-10 15:51:01 +0900

    [KS] FIRST

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <h1>HAPPY</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
