aac1bac95bf0357737298c4eeb32c13232e01576
KS <bye_ok@naver.com>
2020-10-13 09:18:17 +0900

    [login init 006]

--- functions/middlewares/error.js
+++
@@ -1,3 +0,0 @@
-module.exports = (err, req, res, next) => {
-  res.send(err.message)
-}
--- functions/middlewares/verifyToken.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  // console.log(JSON.stringify(req.headers))
-  console.log(req.headers)
-  console.log('here here')
-  next()
-}
